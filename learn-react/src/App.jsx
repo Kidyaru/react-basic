01e2b766364f55a65a7884c8daf6ddb15b445cc2
R4fl1 <raflifnd@gmail.com>
2025-12-09 03:39:28 +0700

    mempelajari dasar component didalam react

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,38 +1,47 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { IconBrandFacebook, IconBrandGithub, IconBrandTiktok, IconBrandTwitter } from '@tabler/icons-react';
+import clsx from 'clsx';
 
-function App() {
-    const [count, setCount] = useState(0);
+export default function App() {
+    const type = 'submit';
+    const onClick = () => console.log('login dengan gaya lain');
 
     return (
-        <>
-            <div>
-                <a href='https://vite.dev' target='_blank'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
-                </a>
+        <div className='bg-slate-900 grid place-content-center min-h-screen'>
+            <div className='flex gap-x-2'>
+                <Button {...{ type, onClick }}>
+                    <IconBrandTiktok />
+                    login
+                </Button>
+                <Button onClick={() => console.log('Aku memencet tombol Register')} type='submit'>
+                    <IconBrandFacebook />
+                    Register
+                </Button>
+                <Button className='bg-pink-600' type='button'>
+                    <IconBrandTwitter />
+                    Login
+                </Button>
+                <Button className='bg-black'>
+                    <IconBrandGithub />
+                    Login
+                </Button>
             </div>
-            <h1>Vite + React</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        </div>
     );
 }
 
-export default App;
+function Button(props) {
+    const { className = 'bg-blue-600', children, text, type = 'submit' } = props;
+
+    return (
+        <button
+            {...props}
+            type={type}
+            className={clsx(
+                className,
+                '${className} [&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1  text-white px-4 py-2 rounded flex items-center gap-1',
+            )}
+        >
+            {text || children}
+        </button>
+    );
+}
