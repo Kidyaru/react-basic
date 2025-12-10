19f07e275fe3f9e010a1b33f653f315b6d9410ab
R4fl1 <raflifndi@gmail.com>
2025-12-10 12:37:17 +0700

    menambahkan branch di laptop untuk perbandingan dengan di pc

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,7 +1,9 @@
 import { IconBrandFacebook, IconBrandGithub, IconBrandTiktok, IconBrandTwitter } from '@tabler/icons-react';
 import clsx from 'clsx';
+import { Fragment } from 'react';
+import Button from './components/button';
 
-export default function App() {
+function App() {
     const type = 'submit';
     const onClick = () => console.log('login dengan gaya lain');
 
@@ -29,19 +31,6 @@ export default function App() {
     );
 }
 
-function Button(props) {
-    const { className = 'bg-blue-600', children, text, type = 'submit' } = props;
 
-    return (
-        <button
-            {...props}
-            type={type}
-            className={clsx(
-                className,
-                '${className} [&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1  text-white px-4 py-2 rounded flex items-center gap-1',
-            )}
-        >
-            {text || children}
-        </button>
-    );
-}
+
+export default App;
